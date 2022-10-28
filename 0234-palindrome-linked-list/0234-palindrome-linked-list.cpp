a6097363e2f150f1f5ed90a211c42b53af7c5a1d
Akshat Srivastava <83699665+Aks9111hat@users.noreply.github.com>
2022-10-28 20:29:35 +0530

    Time: 348 ms (74.03%), Space: 118.1 MB (55.13%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findMiddle(ListNode *head)
+        {
+            ListNode *slow = head;
+            ListNode *fast = head->next;
+            while (fast && fast->next)
+            {
+                fast = fast->next->next;
+                slow = slow->next;
+            }
+            return slow;
+        }
+    ListNode* reverse(ListNode *head)
+    {
+        ListNode *prev = NULL;
+        ListNode *curr = head;
+        ListNode *forward = NULL;
+
+        while (curr)
+        {
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode *head)
+    {
+        if (head->next == NULL)
+            return true;
+
+        ListNode *middle = findMiddle(head);
+
+        middle->next = reverse(middle->next);
+
+        ListNode *temp1 = head;
+        ListNode *temp2 = middle->next;
+        while (temp2)
+        {
+            if (temp1->val != temp2->val)
+                return false;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        return true;
+    }
+};
